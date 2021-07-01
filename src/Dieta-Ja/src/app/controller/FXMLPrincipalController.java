02fa15119f0a19f3a2b3586e96ca967592fa2389
Caio Augusto Barretta <caioabarretta@gmail.com>
2021-07-01 18:22:48 -0300

    Ajuste Chamadas form

--- src/Dieta-Ja/src/app/controller/FXMLPrincipalController.java
+++ src/Dieta-Ja/src/app/controller/FXMLPrincipalController.java
@@ -3,10 +3,50 @@ package app.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import app.Main;
+import app.enums.FXMLScreen;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class FXMLPrincipalController implements Initializable {
 
+	@FXML
+	public void clickCadastrarAlimento(ActionEvent event) throws Exception{
+		Main.switchScreen(FXMLScreen.FXMLAlimentosCadastro);
+	}
+	
+	@FXML
+	public void clickConsultarAlimento(ActionEvent event) throws Exception{
+		Main.switchScreen(FXMLScreen.FXMLAlimentosBusca);
+	}
+	
+	@FXML
+	public void clickCadastrarDieta(ActionEvent event){
+		
+	}
+	
+	@FXML
+	public void clickConsultarDieta(ActionEvent event){
+		
+	}
+	
+	@FXML
+	public void clickCadastrarPaciente(ActionEvent event) throws Exception{
+		Main.switchScreen(FXMLScreen.FXMLPacientesCadastro);
+	}
+	
+	@FXML
+	public void clickConsultarPaciente(ActionEvent event) throws Exception{
+		Main.switchScreen(FXMLScreen.FXMLPacientesConsulta);
+	}
+	
+	@FXML
+	public void clickRegistrarConsumo(ActionEvent event){
+		
+	}
+	
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
