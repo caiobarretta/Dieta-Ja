88f0623ceef5bc38d86e4af988bb07187485bb09
mizialima <mizia.alima@gmail.com>
2021-07-10 00:04:27 -0300

    DDL, DML, DQL, FUNCTIONS, VIEWS, Acessos

---
+++ entrega_final/BD2-EntregaFinal-DietaJa/FUNCTIONS.sql
@@ -0,0 +1,69 @@
+USE DietaJa;
+
+-- FUNCTIONS
+-- CONVERT ENUMS: Sentimento, Refeicao, DiaDaSemana
+DROP FUNCTION IF EXISTS CONVERT_ENUM_SENTIMENTO_STR;
+CREATE FUNCTION CONVERT_ENUM_SENTIMENTO_STR(
+  enumerador INT
+)
+RETURNS VARCHAR(255)
+RETURN 
+(CASE
+	WHEN enumerador = 1 THEN "Muito Satisfeito"
+    WHEN enumerador = 2 THEN "Satisfeito"
+    WHEN enumerador = 3 THEN "Razoavelmente satisfeito"
+    WHEN enumerador = 4 THEN "Um pouco insatisfeito"
+    WHEN enumerador = 5 THEN "Insatisfeito"
+    WHEN enumerador = 6 THEN "Totalmente insatisfeito"
+    END);
+
+DROP FUNCTION IF EXISTS CONVERT_ENUM_REFEICAO_STR;
+CREATE FUNCTION CONVERT_ENUM_REFEICAO_STR(
+  enumerador INT
+)
+RETURNS VARCHAR(255)
+RETURN 
+(CASE
+	WHEN enumerador = 1 THEN "Café da manhã"
+    WHEN enumerador = 2 THEN "Brunch"
+    WHEN enumerador = 3 THEN "Almoço"
+    WHEN enumerador = 4 THEN "Lanche"
+    WHEN enumerador = 5 THEN "Jantar"
+    WHEN enumerador = 6 THEN "Ceia"
+END);
+
+DROP FUNCTION IF EXISTS CONVERT_ENUM_DIA_DA_SEMANA_STR;
+CREATE FUNCTION CONVERT_ENUM_DIA_DA_SEMANA_STR(
+  enumerador INT
+)
+RETURNS VARCHAR(255)
+RETURN 
+(CASE
+	WHEN enumerador = 1 THEN "Segunda-Feira"
+    WHEN enumerador = 2 THEN "Terça-Feira"
+    WHEN enumerador = 3 THEN "Quarta-Feira"
+    WHEN enumerador = 4 THEN "Quinta-Feira"
+    WHEN enumerador = 5 THEN "Sexta-Feira"
+    WHEN enumerador = 6 THEN "Sábado"
+    WHEN enumerador = 7 THEN "Domingo"
+    END);
+
+-- SPLIT 
+DROP FUNCTION IF EXISTS SPLIT_STR;
+CREATE FUNCTION SPLIT_STR(
+  x VARCHAR(255),
+  delim VARCHAR(12),
+  pos INT
+)
+RETURNS VARCHAR(255)
+RETURN REPLACE(SUBSTRING(SUBSTRING_INDEX(x, delim, pos),
+       LENGTH(SUBSTRING_INDEX(x, delim, pos -1)) + 1),
+       delim, '');
+
+-- TESTES FUNCTIONS
+SELECT CONVERT_ENUM_SENTIMENTO_STR(1);
+SELECT CONVERT_ENUM_REFEICAO_STR(2);
+SELECT CONVERT_ENUM_DIA_DA_SEMANA_STR(3);
+SELECT SPLIT_STR("dietaja,podeser", "podeser", 1);
+
+
